2ae58766270432d562918b644a1e8dc1a61c97a4
Matthew Capper <matthew.capper01@gmail.com>
2019-03-10 10:52:02 -0500

    all exercises functional

--- 4.5.1_file_exercises.py
+++ 4.5.1_file_exercises.py
@@ -1,3 +1,4 @@
+import os
 with open('4.5_import_exercises.py') as f:
     lines = f.read()
 lines = lines.split('\n')
@@ -5,16 +6,19 @@ lines = lines.split('\n')
 new_lines = []
 count = 1
 for line in lines:
-        new_lines.append(str(count) + '    ' + line)
-        count += 1
+    new_lines.append(str(count) + '    ' + line)
+    count += 1
 
 [print(line) for line in new_lines]
 
-grocery_list = ['tofu','broccoli','noodles']
+grocery_list = ['tofu', 'broccoli', 'noodles']
+
+
 def make_grocery_list(groceries):
-    with open('my_grocery_list.txt', 'x') as f:
+    with open('my_grocery_list.txt', 'w') as f:
         for item in groceries:
             f.write('%s\n' % item)
+
 
 def show_grocery_list(groce_file):
     with open(groce_file) as f:
@@ -25,15 +29,28 @@ def show_grocery_list(groce_file):
         new_groceries.append(line)
     [print(line) for line in new_groceries]
 
+
 def buy_item():
-    item = input('What item would you like to remove from your grocery list?: ')
-    with open('my_grocery_list.txt', 'x') as groceries:
-        for line in lines:
-            if line.startswith('item'):
-                continue
-            else:
-                new_groceries.append(line)
+    item = input(
+        'What item would you like to remove from your grocery list?: ')
+    new_groceries = []
+    temp_groc_file = 'temp_groceries.txt'
+    with open('my_grocery_list.txt') as groceries:
+        lines = groceries.read()
+    lines = lines.split('\n')
+    for line in lines:
+        if line.startswith(item) or line == '\n':
+            continue
+        else:
+            new_groceries.append(line)
+    with open(temp_groc_file, 'w')as temp_groc:
         for item in new_groceries:
-            string_groceries += str(item) + '\n'
-        f.write(string_groceries)
-        
+            temp_groc.write(str(item) + '\n')
+    os.remove('my_grocery_list.txt')
+    os.rename(temp_groc_file, 'my_grocery_list.txt')
+
+
+make_grocery_list(grocery_list)
+show_grocery_list('my_grocery_list.txt')
+buy_item()
+show_grocery_list('my_grocery_list.txt')
